0f6e8be3b7fb597d194c4639dbdf7a51ee7dcfd3
TruongQK1987 <truongqk1987@gmail.com>
2020-02-13 07:37:52 +0700

    Separe modules

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
 #!/usr/bin/env node
-const reduxSagaCreator = require('./src/redux-saga-creator');
+const reduxSagaCreator = require("./src/redux-saga-creator");
 const yargs = require("yargs");
-const makeDir = require("mk-dir");
 
 yargs
- .command("$0", '', (yargs) => {
-    return yargs.usage("Usage: metanet <command>")
- }, () => {})
- .command("redux-saga <container-name>", "Generate Action, Saga, Reducer in container folder",
-    (yargs) => {
-        return yargs.positional('container-name', {
-                        describe: 'Name of container',
-                        type: 'string',
-                    })
-                    .option("e", { alias: "entity", describe: "Entity name", type: "string"})
-                    .option("i", { alias: "init", describe: "Init basic redux-saga files (store, root of sagas and reducers)", type: "boolean"})
-                    .usage("Usage: redux-saga <container-name> [otpions] [value]")
-    }, (args) => {
-        const containerName = args['container-name'];
-        const isInit = args.init;
-        const folderContainerPath = process.env.PWD + "/src/containers/" + containerName;
-        const generateInContainerFolders = [
-            'actions',
-            'sagas',
-            'reducers'
-        ]
-        const generateEntityFileType = [
-            'action', 'saga', 'reducer'
-        ]
-
-        if (isInit) {
-            reduxSagaCreator.createInitFiles();
-        } else if (containerName) {
-            (async () => {
-                const paths = await Promise.all(generateInContainerFolders.map(folderName => 
-                    makeDir(folderContainerPath + '/' + folderName)
-                ));
-                if (args.entity) {
-                    const entityName = args.entity || containerName.toLowerCase();
-                    generateEntityFileType.forEach(
-                        fileType => reduxSagaCreator.createFile(entityName, containerName, fileType))
-                }
-            })();
-        }
+  .command(
+    "$0",
+    "",
+    yargs => {
+      return yargs.usage("Usage: metanet <command>");
+    },
+    () => {}
+  )
+  .command(
+    "redux-saga <container-name>",
+    "Generate Action, Saga, Reducer in container folder",
+    yargs => {
+      return yargs
+        .positional("container-name", {
+          describe: "Name of container",
+          type: "string"
+        })
+        .option("e", {
+          alias: "entity",
+          describe: "Entity name",
+          type: "string"
+        })
+        .option("i", {
+          alias: "init",
+          describe:
+            "Init basic redux-saga files (store, root of sagas and reducers)",
+          type: "boolean"
+        })
+        .usage("Usage: redux-saga <container-name> [otpions] [value]");
+    },
+    args => {
+      const containerName = args["container-name"];
+      const isInit = args.init;
+      const hasEntity = args.entity;
+      reduxSagaCreator.createFiles(containerName, isInit, hasEntity);
     }
- )
- .argv;
-
-
-
+  ).argv;
--- src/redux-saga-creator/index.js
+++ src/redux-saga-creator/index.js
@@ -1,64 +1,83 @@
-const { lowerCaseFirst } = require('lower-case-first');
-const { upperCaseFirst } = require('upper-case-first');
-const fs = require('fs-extra');
-const path = require('path');
+const { lowerCaseFirst } = require("lower-case-first");
+const { upperCaseFirst } = require("upper-case-first");
+const makeDir = require("mk-dir");
+const fs = require("fs-extra");
+const path = require("path");
 
 const pwd = process.env.PWD; // folder where command start
 
 async function replacePlaceHolders(filePath, entityName) {
-    const fileContent = await fs.readFile(filePath, 'utf8');
+  const fileContent = await fs.readFile(filePath, "utf8");
 
-    const result = fileContent
-        .replace(/'<nameOf>'/g, lowerCaseFirst(entityName))
-        .replace(/'<NameOf>'/g, upperCaseFirst(entityName))
-        .replace(/'<nameof>'/g, entityName.toLowerCase());
+  const result = fileContent
+    .replace(/'<nameOf>'/g, lowerCaseFirst(entityName))
+    .replace(/'<NameOf>'/g, upperCaseFirst(entityName))
+    .replace(/'<nameof>'/g, entityName.toLowerCase());
 
-    await fs.writeFile(filePath, result, 'utf8');
+  await fs.writeFile(filePath, result, "utf8");
 }
 
-const buildPathOfFileInContainer = async (containerName, buildFileName, fileType) => {
-    const fileTypeMap = {
-        action: {
-            subFolder: 'actions',
-            buildFileExtension: 'Action.js',
-            copyFileName: 'action'
-        },
-        saga: {
-            subFolder: 'sagas',
-            buildFileExtension: 'Saga.js',
-            copyFileName: 'saga'
-        },
-        reducer: {
-            subFolder: 'reducers',
-            buildFileExtension: 'Reducer.js',
-            copyFileName: 'reducer'
-        },
+const buildPathOfFileInContainer = async (
+  containerName,
+  buildFileName,
+  fileType
+) => {
+  const fileTypeMap = {
+    action: {
+      subFolder: "actions",
+      buildFileExtension: "Action.js",
+      copyFileName: "action"
+    },
+    saga: {
+      subFolder: "sagas",
+      buildFileExtension: "Saga.js",
+      copyFileName: "saga"
+    },
+    reducer: {
+      subFolder: "reducers",
+      buildFileExtension: "Reducer.js",
+      copyFileName: "reducer"
     }
+  };
 
-    const { subFolder, buildFileExtension, copyFileName } = fileTypeMap[fileType];
-    const filePath = path.join(
-        pwd,
-        'src/containers/' + containerName,
-        subFolder,
-        buildFileName.toLowerCase() + buildFileExtension
-    );
-    await fs.copy(
-        path.join(__dirname, 'templates/redux-saga', copyFileName + '.tpl'),
-        filePath
-      );
-    await replacePlaceHolders(filePath, buildFileName);
-}
-
+  const { subFolder, buildFileExtension, copyFileName } = fileTypeMap[fileType];
+  const filePath = path.join(
+    pwd,
+    "src/containers/" + containerName,
+    subFolder,
+    buildFileName.toLowerCase() + buildFileExtension
+  );
+  await fs.copy(
+    path.join(__dirname, "templates/redux-saga", copyFileName + ".tpl"),
+    filePath
+  );
+  await replacePlaceHolders(filePath, buildFileName);
+};
 
 module.exports = {
-    createFile: async (buildFileName, containerName, fileType) => {
-        buildPathOfFileInContainer(
-            containerName,
-            buildFileName,
-            fileType
-        )
-    },
-    createInitFiles: async() => {
-        
+  createFiles: async (containerName, isInit, hasEntity) => {
+    const folderContainerPath =
+      process.env.PWD + "/src/containers/" + containerName;
+    const generateInContainerFolders = ["actions", "sagas", "reducers"];
+    const generateEntityFileType = ["action", "saga", "reducer"];
+
+    if (isInit) {
+      this.createInitFiles();
+    } else if (containerName) {
+      (async () => {
+        const paths = await Promise.all(
+          generateInContainerFolders.map(folderName =>
+            makeDir(folderContainerPath + "/" + folderName)
+          )
+        );
+        if (hasEntity) {
+          const entityName = hasEntity || containerName.toLowerCase();
+          generateEntityFileType.forEach(fileType =>
+            buildPathOfFileInContainer(containerName, entityName, fileType)
+          );
+        }
+      })();
     }
-}
+  },
+  createInitFiles: async () => {}
+};
